cd3026f01dd6a9f7432cb82e5967d80f4fef9d30
danielbrugio <daniel.brugioni@hotmail.com>
2022-01-31 01:01:00 -0300

    create-npm

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -1,4 +1,4 @@
-//import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { TextInput } from '../TextInput/TextInput'
 import { OptionAutocomplete, StyledAutocomplete } from './StyledAutocomplete'
